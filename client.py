1b4981d5ee15d81d2f554dfbdfd13a0bf4f32b6a
shashihacks <shashi.kumar@snipclip.com>
2021-05-30 21:13:40 +0200

    refactored initial key exchange

--- client.py
+++ client.py
@@ -3,73 +3,77 @@ import math
 import random
 global prime, root
 
-if __name__ == "__main__":
-    host = "127.0.0.1"
-    port = 4455
-    addr = (host, port)
 
-    """ Creating the UDP socket """
-    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+host = "127.0.0.1"
+port = 4455
+addr = (host, port)
 
-    # Key Information
-    def secretNumber ():
-        secret = int(random.randint(0,100))
-        return secret
-    mySecretKey = 96
-    root = 3 # agreed root
-    prime = 17
-    # server public key (variable) 
-    myPublicKey = 1
-    def calculateSharedKey(receivedPublicKey):
-        print("calculating shared key")
-        mySharedKey = (int(receivedPublicKey) ** mySecretKey) % prime
-        print("client shared key" , mySharedKey)
+""" Creating the UDP socket """
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # Request file 
-    while True:
+# Key Information
+def secretNumber ():
+    secret = int(random.randint(0,100))
+    return secret
+mySecretKey = 96
+root = 3 # agreed root
+prime = 17
+# server public key (variable) 
+myPublicKey = 1
+def calculateSharedKey(receivedPublicKey):
+    print("calculating shared key")
+    mySharedKey = (int(receivedPublicKey) ** mySecretKey) % prime
+    print("client shared key" , mySharedKey)
 
-        data = input("Enter your input: ")
-        # filename = data
+# Request file 
 
-        if data == "!EXIT":
-            data = data.encode("utf-8")
-            client.sendto(data, addr)
-            print("Disconneted from the server.")
-            client.close()
-            exit()
 
+askKey = "KEY".encode("utf-8")
+client.sendto(askKey, addr)
+data, addr = client.recvfrom(1024)
+data = data.decode("utf-8")
+if(data[:3]=='KEY'): 
+    print(f"{data}")
+    getKeyFromData = f"{data}"[4:]
+    calculateSharedKey(getKeyFromData)
+
+while True:
+
+    data = input("Enter your input: ")
+    filename = data
+
+    if data == "exit":
         data = data.encode("utf-8")
         client.sendto(data, addr)
+        print("Disconneted from the server.")
+        client.close()
+        exit()
 
-        data, addr = client.recvfrom(1024)
-        data = data.decode("utf-8")
-        # print(f"{data}")
-        # calculateSharedKey(f"{data}")
-             
-        if(data[:3]=='KEY'): 
-            print(f"{data}")
-            getKeyFromData = f"{data}"[4:]
-            print(getKeyFromData)
-            calculateSharedKey(getKeyFromData)
-        elif(data[:6]=='EXISTS'):
-            filesize = int(data[6:])
-            print(filesize, "filesize")
-            f = open('new_'+data, 'wb')
+    data = data.encode("utf-8")
+    client.sendto(data, addr)
+
+    data, addr = client.recvfrom(1024)
+    data = data.decode("utf-8")
+
+    if(data[:6]=='EXISTS'):
+        filesize = int(data[6:])
+        print(filesize, "filesize")
+        f = open('new_'+filename, 'wb')
+        data,addr = client.recvfrom(1024)
+        totalRecv = len(data)
+        print("writing data")
+        f.write(data)
+        while totalRecv < filesize:
             data,addr = client.recvfrom(1024)
-            # data = data.decode("utf-8")
-            totalRecv = len(data)
+            totalRecv+=len(data)
             f.write(data)
-            while totalRecv < filesize:
-                data,addr = client.recvfrom(1024)
-                totalRecv+=len(data)
-                f.write(data)
-                # print("{0:.2f}".format((totalRecv/float(filesize))*100)+ "% Done")
-            print("download complete !!")
+            # print("{0:.2f}".format((totalRecv/float(filesize))*100)+ "% Done")
+        print("download complete !!")
 
-            f.close()
-            # client.close()
-        else:
-            print("file does'nt exist")
-    # client.close()
+        f.close()
+        # client.close()
+    else:
+        print("file does'nt exist")
+# client.close()
 
     
--- server.py
+++ server.py
@@ -3,71 +3,74 @@ import os
 import math
 import random
 global prime, root
-# from Crypto.Cipher import AES
+from Crypto.Cipher import AES
+from huffman import HuffmanCoding
+import sys
 
-if __name__ == "__main__":
-    host = "127.0.0.1"
-    port = 4455
-    addr = (host, port)
-    """ Creating the UDP socket """
-    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    """ Bind the host address with the port """
-    server.bind((host, port))
 
-    def secretNumber ():
-        secret = int(random.randint(0,100))
-        return secret
+host = "127.0.0.1"
+port = 4455
+addr = (host, port)
+""" Creating the UDP socket """
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # key information
-    clientPublicKey = 1 #bob
-    prime = 17
-    root =3
-    mySecretNumber = 89
-    myPublicKey = 14
+""" Bind the host address with the port """
+server.bind((host, port))
 
-    # calculateSharedKey
-    mySharedKey = (clientPublicKey ** mySecretNumber) % prime
-    print("server shared Key", mySharedKey)
+def secretNumber ():
+    secret = int(random.randint(0,100))
+    return secret
 
-    def retriveFile(filename):
-        print(filename, "exists")
-        server.sendto(("EXISTS " +str(os.path.getsize(filename))).encode("utf-8"), addr)
-        with open(filename, 'rb') as f:
+# key information
+clientPublicKey = 1 #bob
+prime = 17
+root =3
+mySecretNumber = 89
+myPublicKey = 14
+
+# calculateSharedKey
+mySharedKey = (clientPublicKey ** mySecretNumber) % prime
+print("server shared Key", mySharedKey)
+
+def retriveFile(filename):
+    print(filename, "exists")
+    server.sendto(("EXISTS " +str(os.path.getsize(filename))).encode("utf-8"), addr)
+    with open(filename, 'rb') as f:
+        bytesToSend = f.read(1024)
+        server.sendto(bytesToSend, addr)
+        while(bytesToSend):
+            print("sending", bytesToSend)
             bytesToSend = f.read(1024)
             server.sendto(bytesToSend, addr)
-            while(bytesToSend):
-                print("sending", bytesToSend)
-                bytesToSend = f.read(1024)
-                server.sendto(bytesToSend, addr)
-            f.close()
-                
-        # print("closing") 
-        # server.close()
+        f.close()
+            
+    # print("closing") 
+    # server.close()
 
-    def sendPublicKey():
-        print("Sending Public key")
-        keyInfo = 'KEY ' + str(myPublicKey)
-        server.sendto(keyInfo.encode("utf-8"), addr)
-    while True:
-        data, addr = server.recvfrom(1024)
-        data = data.decode("utf-8")
-        print(data)
+def sendPublicKey():
+    print("Sending Public key")
+    keyInfo = 'KEY ' + str(myPublicKey)
+    server.sendto(keyInfo.encode("utf-8"), addr)
+while True:
+    data, addr = server.recvfrom(1024)
+    data = data.decode("utf-8")
+    print(data)
 
-        if data == "!EXIT":
-            print("Client disconnected.")
-            break
+    if data == "exit":
+        print("Client disconnected.")
+        break
 
-        print(f"Client: {data}")
-        data = f"{data}"
-        print(data)
+    print(f"Client: {data}")
+    data = f"{data}"
+    print(data)
 
-        # deffie hellman
+    # deffie hellman
 
-        if(data=="KEY"):
-            sendPublicKey()
-        else:
+    if(data=="KEY"):
+        sendPublicKey()
+    else:
 
-        # File retrive and send
-            retriveFile(data)
-        # server.close()
+    # File retrive and send
+        retriveFile(data)
+    # server.close()
